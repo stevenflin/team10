9351f3cad570ef90011b764790615f17261fc97f
AustinHawk <austinhawkins70@yahoo.com>
2016-08-02 11:24:50 -0400

    connect instagram deleted

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,6 @@ router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Express' });
 });
 
-router.get('/connect/instagram', passport.authorize('instagram', { scope : 'email' }));
-
-router.get('/connect/instagram/callback',
-    passport.authorize('instagram', {
-        successRedirect : '/profile',
-        failureRedirect : '/'
-    }));
-
 
 
 module.exports = router;
