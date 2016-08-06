1f3090d1dbf915f806ccfb18ad178158bc442a81
AustinHawk <austinhawkins70@yahoo.com>
2016-08-06 13:39:12 -0400

    check

--- test/ig.js
+++ test/ig.js
@@ -4,13 +4,13 @@ console.log("AT:", process.env.AT);
 console.log("ID:", process.env.ID);
 console.log("INSTAGRAM_CLIENT_ID:", process.env.INSTAGRAM_CLIENT_ID);
 console.log("INSTAGRAM_CLIENT_SECRET:", process.env.INSTAGRAM_CLIENT_SECRET);
-var parkaplace = "440696783.81b5f2a.5695d0ac66d64e74ad13a5247ea31176";
-ig.use({ access_token: "440696783.81b5f2a.5695d0ac66d64e74ad13a5247ea31176" });
-var bigArr = [];
+var parkaplace = process.env.AT;
 
 function instagramInformation(req){
-	ig.user_media_recent("440696783", {cursor: 30}, function(err, medias, pagination, remaining, limit) {
-		// console.log("medias", medias)
+	var bigArr = [];
+	ig.use({ access_token: process.env.AT });
+	ig.user_media_recent(process.env.ID, {cursor: 30}, function(err, medias, pagination, remaining, limit) {
+		console.log("medias", medias)
 		// console.log("pagination", pagination);
 	 	console.log("medias number", medias.length);
 	 	bigArr = bigArr.concat(medias);
--- test/twitter.js
+++ test/twitter.js
@@ -1,17 +1,22 @@
 var Twitter = require('twitter');
 
-var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
+
+function twitterInformation(req){
+	var client = new Twitter({
+	  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+	  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+	});
+	var params = {user_id: process.env.TWITTER_ID};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    console.log(tweets);
+	  }
+	});
+}
+
+twitterInformation();
 
 // client.get('favorites/list', function(error, tweets, response) {
 //   if(error) throw error;
