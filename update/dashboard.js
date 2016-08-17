c2e3dd6738a1551253f7fc6e4bb901fe2ccac1b3
stevenflin <steven.lin@stern.nyu.edu>
2016-08-17 14:40:28 -0400

    stuff

--- update/dashboard.js
+++ update/dashboard.js
@@ -58,7 +58,7 @@ function getPosts(id) {
 	return new Promise(function(masterResolve, masterReject) {
 		var platforms = ['youtube', 'instagram', 'vine', 'twitter', 'facebook'];
 		Profile.findOne({userId: id}, function(err, profile) {
-			if (err) return next(err);
+			if (err) return masterReject(err);
 			platforms = platforms.map(function(p) {
 				return new Promise(function(resolve, reject) {
 					Post.find({profileId: profile._id, type: p})
@@ -91,7 +91,7 @@ function getPosts(id) {
 							}
 							// console.log('growth growth growth...........', growth);
 							return growth;
-						})
+						});
 
 						resolve({
 							type: p,
@@ -124,7 +124,68 @@ function getPosts(id) {
 	});
 }
 
+function checkAdmin(user) {
+	return new Promise(function(resolve, reject) {
+		if(user.isAdmin) {
+			User.find((err, users) => {
+				if (err) return reject(err);
+				var userArray = users.map((user) => {return {id: user._id, username: user.username}})
+				resolve(userArray)
+			});
+		} else {
+			resolve([]);
+		}
+	});
+}
+
+function getPlatformPosts(id, platform) {
+	return new Promise(function(resolve, reject) {
+		Profile.findOne({userId: id}, function(err, profile) {
+			if (err) return reject(err);
+			ProfileSnapshot.find({platform: platform, profileId: profile._id}, function(err, profileSnaps) {
+				if (err) return reject(err);
+				// console.log('snap snap snap..........', profileSnaps);
+				Post.find({type: platform, profileId: profile._id})
+				.sort({'date': -1})
+				.limit(6)
+				.populate('snapshots')
+				.lean()
+				.exec(function(err, posts) {
+					if (err) return reject(err);
+					// console.log('post post post..........', Object.keys(posts[0].snapshots[0]));
+					// res.send('hi')
+					var change = posts.map((post) => {
+						var change = {};
+						var snaps = post.snapshots;
+						for (var key in snaps[0]) {
+							if (!change[key]) {
+								// console.log('key key key key key key..........', key)
+								change[key] = [];
+								for (var i = 0; i < snaps.length; i++) {
+									change[key].push(snaps[i][key]);
+								}
+							}
+						}
+						// console.log('change change change change........', change);
+						return change;
+					});
+					resolve({
+						posts: posts.map((post) => {
+							var d = new Date(post.date);
+							post.date = (d.getMonth()+1) + '/' + d.getDate() + '/'+d.getFullYear();
+							return post;
+						}),
+						change: change
+					});
+				});
+			});
+		});
+	});
+}
+
 module.exports = {
 	getPosts: getPosts,
-	getGeneral: getGeneral
+	getGeneral: getGeneral,
+	checkAdmin: checkAdmin,
+	getPlatformPosts: getPlatformPosts
 }
