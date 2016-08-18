7b35e050c158c93295693de64a0aacacc6ef1016
AustinHawk <austinhawkins70@yahoo.com>
2016-08-18 16:47:28 -0400

    saving to profile

--- routes/index.js
+++ routes/index.js
@@ -227,14 +227,26 @@ router.post('/snapchat', function(req, res, next) {
 	Profile.findOne({userId: req.user._id}, function(err, profile) {
 		if (err) return next(err);
 		// console.log('profile..........', profile)
-		('integrate', {
-			userId: req.user._id,
-			profile
+		profile.snapchat.displayName = req.body.snapHandle;
+		profile.snapchat.followers = req.body.snapFollowers;
+		profile.save(function(err) {
+			if(err) return next(err);
+			res.redirect('/dashboard')
 		});
 	});
 });
 
 router.post('/music', function(req, res, next) {
+	Profile.findOne({userId: req.user._id}, function(err, profile) {
+		if (err) return next(err);
+		// console.log('profile..........', profile)
+		profile.music.displayName = req.body.musicHandle;
+		profile.music.followers = req.body.musicFollowers;
+		profile.save(function(err) {
+			if(err) return next(err);
+			res.redirect('/dashboard')
+		});
+	});
 
 });
 
